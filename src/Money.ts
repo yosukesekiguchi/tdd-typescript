f384ea2d025bc3d9186812b8753f103bba0b6c7a
yosuke-sekiguchi <yosuke.s.5975@icloud.com>
2020-07-26 20:57:58 +0900

    ch.7
    [memo]
    - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor

--- src/Money.ts
+++ src/Money.ts
@@ -5,8 +5,9 @@ export class Money {
     this.amount = amount;
   }
   
-  public equals(object: Money): boolean {
-    const money: Money = object;
-    return this.amount === money.amount;
+  public equals(object: any): boolean {
+    const money = object as Money;
+    return this.amount === money.amount 
+      && this.constructor.name === money.constructor.name;
   }
 }
--- src/__test__/money.test.ts
+++ src/__test__/money.test.ts
@@ -13,6 +13,7 @@ describe("money", () => {
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
     expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
     expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
   })
 
   it("Multiplication", () => {
