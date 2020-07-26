056b3e097adf56106147e60c2b050797fa857a96
yosuke-sekiguchi <yosuke.s.5975@icloud.com>
2020-07-26 21:40:39 +0900

    ch.8
    [memo]
    - 原因不明のエラー: Class extends value undefined is not a constructor or null
    - extendsが原因っぽい? interfaceを実装すればいいのかもしれないが、abstractでやりたい時はどうすればいいのか

--- src/Money.ts
+++ src/Money.ts
@@ -4,10 +4,48 @@ export class Money {
   constructor(amount: number) {
     this.amount = amount;
   }
+
+  public times(multiplier: number): Money {
+    return new Money(8);
+  };
   
   public equals(object: any): boolean {
     const money = object as Money;
     return this.amount === money.amount 
       && this.constructor.name === money.constructor.name;
+  }
+
+  static dollar(amount: number): Money {
+    return new Dollar(amount);
+  }
+
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
+}
+
+
+export class Dollar extends Money {
+
+  constructor(amount: number) {
+    super(amount);
+    this.amount = amount;
+  }
+
+  public times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+
+export class Franc extends Money {
+
+  constructor(amount: number) {
+    super(amount);
+    this.amount = amount;
+  }
+
+  public times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
   }
 }
