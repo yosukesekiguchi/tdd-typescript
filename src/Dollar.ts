dbeb245e72a658f501750b2255a2ab962056e36e
yosuke-sekiguchi <yosuke.s.5975@icloud.com>
2020-07-26 17:58:36 +0900

    ch.2

--- src/Dollar.ts
+++ src/Dollar.ts
@@ -5,7 +5,7 @@ export class Dollar {
     this.amount = amount;
   }
 
-  public times(multiplier: number): void {
-    this.amount *= multiplier;
+  public times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
   }
 }
--- src/__test__/money.test.ts
+++ src/__test__/money.test.ts
@@ -3,8 +3,12 @@ import { Dollar } from "../Dollar";
 describe('money', () => {
 it('multiplication', () => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toEqual(10);
+    
+    let product = five.times(2);
+    expect(product.amount).toEqual(10);
+
+    product = five.times(3);
+    expect(product.amount).toEqual(15);
   });
 });
 
